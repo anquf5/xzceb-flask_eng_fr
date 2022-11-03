90b96caf67b8333b5fbb3ebacb717cf9ed9e8948
anquf5 <anquf5@gmail.com>
2022-11-03 17:47:08 -0400

    Finan changes

--- final_project/server.py
+++ final_project/server.py
@@ -1,6 +1,23 @@
 from machinetranslation import translator
 from flask import Flask, render_template, request
 import json
+import os
+from ibm_watson import LanguageTranslatorV3
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from dotenv import load_dotenv
+
+load_dotenv()
+url = os.environ['url']
+apikey = os.environ['apikey']
+
+
+# Prepare the Authenticator
+authenticator = IAMAuthenticator(apikey)
+language_translator = LanguageTranslatorV3(
+    version='2022-11-03',
+    authenticator=authenticator
+)
+language_translator.set_service_url(url)
 
 app = Flask("Web Translator")
 
